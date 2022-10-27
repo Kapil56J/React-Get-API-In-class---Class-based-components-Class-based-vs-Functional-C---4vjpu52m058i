a7111ea1e190d25fbcdeaf25a48c770de03a083f
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-10-27 19:07:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,14 @@ import { useEffect , useState } from 'react';
  */
 
 function App() {
-
   const [data, setData] = useState([]);
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((response) => {
+        setData(response);
+      });
+  }, []);
   /**
    * fetch data from api on mount. 
    */
